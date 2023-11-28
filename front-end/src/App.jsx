8860288678242b3fe9d19d2e353af9762ac6baad
Supun Seneviratne <supunseneviratne@gmail.com>
2023-11-28 10:58:35 +0530

    Initial commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import "./App.css"
+const App = () => {
+  return (
+
+    
+        <div className="navigator">
+          <div className="div">
+                      <a href="https://www.masholdings.com" className="overlap" target="_blank" rel="noopener noreferrer">
+              <div>
+                <div className="text-wrapper">OCTIS</div>
+                <p className="p">
+                  Lorem ipsum dolor sit amet, consectetur adipisc elit, sed do eiusmod tempor incididunt ut labore et.
+                </p>
+              </div>
+            </a>
+
+            <a href="https://www.masholdings.com" className="overlap-group" target="_blank" rel="noopener noreferrer">
+              <div>
+                <div className="text-wrapper-2">VR</div>
+                <p className="text-wrapper-3">
+                  Lorem ipsum dolor sit amet, consectetur adipisc elit, sed do eiusmod tempor incididunt ut labore et.
+                </p>
+              </div>
+            </a>
+
+            <div className="overlap-2">
+              <div className="text-wrapper-4">The Navigator</div>
+              <p className="text-wrapper-5">
+                Lorem ipsum dolor sit amet, consectetur adipisc elit, sed do eiusmod tempor incididunt ut labore et.
+              </p>
+            </div>
+                      <a href="https://www.google.com" className="div-wrapper" target="_blank" rel="noopener noreferrer">
+            <div className="text-wrapper-6">OCTIS</div>
+          </a>
+
+          <a href="https://www.masholdings.com/" className="overlap-3" target="_blank" rel="noopener noreferrer">
+            <div className="text-wrapper-7">VR</div>
+          </a>
+
+          <a href="https://www.masholdings.com/" className="overlap-group-2" target="_blank" rel="noopener noreferrer">
+            <div className="text-wrapper-8">Home</div>
+          </a>
+
+          <a href="https://www.masholdings.com/" className="overlap-4" target="_blank" rel="noopener noreferrer">
+            <div className="text-wrapper-9">Summary</div>
+          </a>
+
+            <img
+              className="the-navigator-logo"
+              alt="The navigator logo"
+              src={('./src/assets/The_Navigator_Logo_2-removebg-preview.png')}
+            />
+            <a href="https://www.masholdings.com" className="overlap-5" target="_blank" rel="noopener noreferrer">
+              <div>
+                <div className="text-wrapper-10">INSIGHT PORTAL</div>
+                <p className="text-wrapper-11">
+                  Lorem ipsum dolor sit amet, consectetur adipisc elit, sed do eiusmod tempor incididunt ut labore et.
+                </p>
+              </div>
+            </a>
+
+                    <a href="https://www.masholdings.com" className="overlap-6" target="_blank" rel="noopener noreferrer">
+          <div>
+            <div className="text-wrapper-12">SUMMARY</div>
+            <p className="text-wrapper-13">
+              Lorem ipsum dolor sit amet, consectetur adipisc elit, sed do eiusmod tempor incididunt ut labore et.
+            </p>
+          </div>
+        </a>
+
+          </div>
+        </div>
+      
+  );
+};
+
+  
+
+
+export default App
